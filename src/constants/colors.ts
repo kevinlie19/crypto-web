8204d735b80b1e4ee04d191c78e0a7554f606c8e
Irene Clarissa <ireneclarissa@Irenes-MacBook-Pro.local>
2023-07-14 14:32:16 +0700

    add colors and fonts constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+export const PURE_BLACK = '#000000';
+export const BLACK = '#0B0A0A';
+
+export const GRAY = '#929396';
+export const LIGHT_GRAY = '#F2F2F2';
+export const BORDER = '#E5E7EB';
+export const GREEN = '#25A764';
+export const RED = '#E54040';
+export const BLUE = '#0A68F4';
+export const LIGHT_BLUE = '#EDF4FE';
---
+++ src/constants/fonts.ts
@@ -0,0 +1,6 @@
+export const FONT_FAMILY_INTER = 'Inter';
+
+export const FONT_WEIGHT_NORMAL = '400';
+export const FONT_WEIGHT_MEDIUM = '500';
+export const FONT_WEIGHT_SEMI_BOLD = '600';
+export const FONT_WEIGHT_BOLD = '700';
